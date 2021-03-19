bb0f6f33a5e140cf26cff78977705e4817620570
YergalymS <78316319+YergalymS@users.noreply.github.com>
2021-03-19 10:22:22 +0600

    created API

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class ApiController extends Controller
+{
+    public function index(Request $request) {
+        $posts = Post::all();
+
+        return response($posts, 200);
+    }
+
+    public function get_detail(Request $request) {
+        $post = Post::find($request->post_id);
+
+        if ($post == null) {
+            return response(['message' => 'No such data!'], 404);
+        }
+        return response($post, 200);
+    }
+}
